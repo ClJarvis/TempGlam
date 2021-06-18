f778fe9355de4695db7b646d2836f9f32f01cb45
Chris <clj@ollure.com>
2021-06-18 01:16:13 -0500

    adding firebase

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FirebaseContext from './context/firebase';
+import { firebase, FieldValue } from './lib/firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// client side render app: react (cra)
-// firebase database
-// react loading skelton
-// tail wind
-
-// architecture folder structure
-// src
-// componets
-// context
-// helpers
-// hooks
-// lib
-// pages
-// services
+ReactDOM.render(
+	<FirebaseContext.Provider value={{ firebase, FieldValue }}>
+		<App />
+	</FirebaseContext.Provider>,
+	document.getElementById('root')
+	);
---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyCiybQIdPgmr7TflCD4n-_E7PAWC3cNnMA',
+  authDomain: 'tempgram-e3f79.firebaseapp.com',
+  projectId: 'tempgram-e3f79',
+  storageBucket: 'tempgram-e3f79.appspot.com',
+  messagingSenderId: '635425378025',
+  appId: '1:635425378025:web:b40706af695fc9c28e2a78'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+export { firebase, FieldValue };
