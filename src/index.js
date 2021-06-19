dc88a040ef83ca8f71da2a1421b53387696ba6df
Chris <clj@ollure.com>
2021-06-18 21:58:53 -0500

    fixed error

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
 
-
 ReactDOM.render(
-	<FirebaseContext.Provider value={{ firebase, FieldValue }}>
-		<App />
-	</FirebaseContext.Provider>,
-	document.getElementById('root')
-	);
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+);
