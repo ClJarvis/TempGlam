b20bd1b5ad70b3a3035c79a1e60e543309fb3fc8
Chris <clj@ollure.com>
2021-06-21 16:02:01 -0500

    builsing login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,26 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
 export default function Login() {
-  return <p>This is the TempGlam Login page</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const { emailAddress, setEmailAddress } = useState('');
+  const { password, setPassword } = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - TempGlam';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md item-center h-screen">
+      <p>This is the TempGlam Login page</p>
+    </div>
+  );
 }
